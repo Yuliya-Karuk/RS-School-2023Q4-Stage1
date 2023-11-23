0ca2855dd6d1e00e5716729c4ea5168f9bdbea3a
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-11-23 15:25:52 +0400

    feat: impliment adaptivity for header and burger Menu

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -1,3 +1,9 @@
+import Burger from './js/burger';
 import selfEsteem1 from './js/selfEsteem';
 
 console.log(selfEsteem1);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = new Burger();
+  burger.bindListeners();
+});
