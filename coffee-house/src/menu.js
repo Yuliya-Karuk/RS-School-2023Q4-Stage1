4c41ed46b720b8c1f97928fe8519c776db5b22d5
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-11-24 12:07:18 +0400

    feat: add adaptivity for Menu page

--- coffee-house/src/menu.js
+++ coffee-house/src/menu.js
@@ -1,6 +1,10 @@
 import Catalog from './js/catalog';
+import Burger from './js/burger';
 
 document.addEventListener('DOMContentLoaded', () => {
   const catalog = new Catalog('coffee');
+  catalog.bindListeners();
   catalog.renderCatalog();
+  const burger = new Burger();
+  burger.bindListeners();
 });
