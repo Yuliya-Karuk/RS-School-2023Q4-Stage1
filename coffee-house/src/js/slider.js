a5a7326829bc9daa53f9bcbc723b01167b1485c3
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-11-29 18:10:30 +0400

    feat: add functions for mobile touch

--- coffee-house/src/js/slider.js
+++ coffee-house/src/js/slider.js
@@ -1,12 +1,35 @@
 class Slider {
   constructor() {
+    this.sliderParent = document.querySelector('.slider');
     this.sliderButtonLeft = document.querySelector('.slider__btn_left');
     this.sliderButtonRight = document.querySelector('.slider__btn_right');
     this.slider = document.querySelector('.slider_list');
-    this.paginationBtns = document.querySelectorAll('.pagination__btn');
-    this.paginationBtnActive = document.querySelector('.pagination__btn_active');
-    this.sliderItems = 3;
+    this.sliderItems = document.querySelectorAll('.slider__item');
+    this.paginationBars = document.querySelectorAll('.pagination__bar');
     this.currentSlide = 0;
+    this.paginationBtnActive = this.paginationBars[this.currentSlide].firstElementChild;
+    this.timeStep = 10;
+    this.time = 5000;
+    this.filledSpace = 0;
+    this.isSwiped = false;
+  }
+
+  fillPagination(start = true) {
+    if (start) {
+      this.interval = setInterval(this.fillStep.bind(this), 500);
+    }
+    if (!start) {
+      clearInterval(this.interval);
+    }
+  }
+
+  fillStep() {
+    if (this.filledSpace >= 100) {
+      this.changeSlide(1);
+    } else {
+      this.filledSpace += this.timeStep;
+      this.paginationBtnActive.style.width = `${this.filledSpace}%`;
+    }
   }
 
   findNextSlide(offset) {
@@ -15,32 +38,51 @@ class Slider {
     if (this.currentSlide < 0) this.currentSlide = 2;
   }
 
+  changePgnBar() {
+    for (let i = 0; i < this.paginationBars.length; i += 1) {
+      this.paginationBars[i].firstElementChild.style.width = 0;
+    }
+    this.paginationBtnActive = this.paginationBars[this.currentSlide].firstElementChild;
+  }
+
+  changeSlide(offset) {
+    this.findNextSlide(offset);
+    this.move();
+    this.changePgnBar();
+    this.filledSpace = 0;
+  }
+
   move() {
     this.slider.style.transform = `translateX(-${this.currentSlide * 100}%)`;
   }
 
-  restartSlider() {
-    this.generateConst();
-    this.currentSlide = 0;
-    this.fillPagination();
-    this.move();
-  }
-
   bindListeners() {
-    const context = this;
-    document.addEventListener('DOMContentLoaded', () => context.fillPagination());
-    // window.addEventListener('resize', () => context.restartSlider());
+    this.sliderButtonRight.addEventListener('click', () => this.changeSlide(1));
+    this.sliderButtonLeft.addEventListener('click', () => this.changeSlide(-1));
 
-    this.sliderButtonRight.addEventListener('click', () => {
-      this.findNextSlide(1);
-      this.move();
-      // context.fillPagination();
+    for (let i = 0; i < this.sliderItems.length; i += 1) {
+      this.sliderItems[i].addEventListener('mouseover', () => this.fillPagination(false));
+      this.sliderItems[i].addEventListener('mouseout', () => this.fillPagination());
+    }
+
+    this.sliderParent.addEventListener('touchstart', (e) => {
+      this.touchStart = e.targetTouches[0].clientX;
+      this.fillPagination(false);
     });
 
-    this.sliderButtonLeft.addEventListener('click', () => {
-      this.findNextSlide(-1);
-      this.move();
-      // context.fillPagination();
+    this.sliderParent.addEventListener('touchmove', (e) => {
+      this.touchEnd = e.changedTouches[0].clientX;
+      this.isSwiped = true;
+    });
+
+    this.sliderParent.addEventListener('touchend', (e) => {
+      if (e.cancelable && this.isSwiped) {
+        e.preventDefault();
+        const side = this.touchStart - this.touchEnd > 0 ? 1 : -1;
+        this.changeSlide(side);
+        this.isSwiped = false;
+      }
+      this.fillPagination();
     });
   }
 }
