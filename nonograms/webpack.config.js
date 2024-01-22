223cf9b00bccfaffc696b33d39e6899e75d2acb2
Yuliya-Karuk <karuk.yuliya@gmail.com>
2024-01-22 17:07:02 +0400

    feat: configure webpack and linters for project

---
+++ nonograms/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [path.resolve(__dirname, 'src', 'index.js'), './src/sass/index.scss'],
+  output: {
+    filename: 'script.js',
+    path: path.join(__dirname, '/dist'),
+    clean: true,
+  },
+  watch: true,
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+      {
+        test: /\.(woff2?|ttf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext][query]',
+        },
+      },
+      {
+        test: /\.(html)$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new ESLintPlugin(),
+  ],
+};
